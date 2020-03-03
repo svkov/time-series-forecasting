011b995c16b6a237d195d63cd5ad14125b96b790
Svyatoslav <kovalev.svyatoslav42@gmail.com>
2020-03-03 23:36:54 +0300

    train-test validating (mae, mse, mape) fix forecasting

--- forecasting/__init__.py
+++ forecasting/__init__.py
@@ -3,11 +3,12 @@ from datetime import datetime
 import numpy as np
 from numpy import fft
 import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
 
 
 def fourierExtrapolation(x, n_predict, n_harm=10):
     n = x.size
-    # n_harm = 10                     # number of harmonics in model
     t = np.arange(0, n)
     p = np.polyfit(t, x, 1)  # find linear trend in x
     x_notrend = x - p[0] * t  # detrended x
@@ -26,11 +27,11 @@ def fourierExtrapolation(x, n_predict, n_harm=10):
     return restored_sig + p[0] * t
 
 
-def forecaft_df(df, n_pred, n_harm=10000, start_from=60000, last_n_values=None):
+def forecaft_df_to_future(df, n_pred, n_harm=10000, start_from=60000, last_n_values=None):
     val = df['avg'].values[start_from:]
     y1 = val
     y2 = fourierExtrapolation(val, n_predict=n_pred, n_harm=n_harm)
-    y2_hat = abs(y2[-n_pred] - y1[-1]) + y2[-n_pred:]
+    y2_hat = y2[-n_pred:] - (y2[-n_pred] - y1[-1])
     return y2_hat
 
 
@@ -42,3 +43,28 @@ def get_indexes_for_prediction(df, n_pred, freq='H'):
     ts = int(df.iloc[-1].time)
     date = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     return pd.date_range(date, periods=n_pred, freq=freq)
+
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+def get_forecast_test(df, n_harm=10000, start_from=0, train_size=0.8):
+    val = df['avg'].values[start_from:]
+    train, test = train_test_split(val, shuffle=False, train_size=train_size)
+    n_pred = len(test)
+    y_forecast = fourierExtrapolation(train, n_predict=n_pred, n_harm=n_harm)[-n_pred:]
+    y_forecast = y_forecast - (y_forecast[0] - train[-1])
+    return train, test, y_forecast
+
+
+def calculate_metrics(y_true, y_pred):
+    return {'MAPE': mean_absolute_percentage_error(y_true, y_pred),
+            'MAE': mean_absolute_error(y_true, y_pred),
+            'MSE': mean_squared_error(y_true, y_pred)}
+
+
+def forecast_train_test_df(df, n_harm=10000, start_from=0, train_size=0.8):
+    train, test, y_forecast = get_forecast_test(df, n_harm, start_from, train_size)
+    return calculate_metrics(test, y_forecast)
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import dash_html_components as html
 import pandas as pd
 from dash.dependencies import Input, Output
 
-from forecasting import forecaft_df, get_indexes_for_prediction
+from forecasting import forecaft_df_to_future, get_indexes_for_prediction, forecast_train_test_df, get_forecast_test, \
+    calculate_metrics
 
 colors = {
     'background': '#111111',
@@ -33,11 +34,11 @@ def load_data(start_from=80000):
 
 def forecast(n_pred, harm=10000):
     global df
-    forecasted = forecaft_df(df, n_pred=n_pred, n_harm=harm, start_from=0)
+    forecasted = forecaft_df_to_future(df, n_pred=n_pred, n_harm=harm, start_from=0)
     return forecasted
 
 
-def get_figure(df, forecasted):
+def get_figure_to_future(df, forecasted):
     return {
         'data': [{'x': df.index, 'y': df['avg'], 'type': 'plot'},
                  {'x': get_indexes_for_prediction(df, n_pred=n_pred), 'y': forecasted, 'type': 'plot'}],
@@ -52,21 +53,53 @@ def get_figure(df, forecasted):
     }
 
 
-def get_graph(df, forecasted):
-    return dcc.Graph(id='plot', figure=get_figure(df, forecasted))
+def get_figure_train_test(df, **kwargs):
+    train, test, forecasted = get_forecast_test(df, **kwargs)
+    train_index = df.iloc[:len(train)].index
+    test_index = df.iloc[len(train):].index
+    return {
+        'data': [{'x': train_index, 'y': train, 'type': 'plot', 'name': 'train'},
+                 {'x': test_index, 'y': test, 'type': 'plot', 'name': 'test'},
+                 {'x': test_index, 'y': forecasted, 'type': 'plot', 'name': 'forecast'}],
+        'layout': {
+            'title': 'Train-Test validation',
+            'plot_bg_color': colors['background'],
+            'paper_bgcolor': colors['background'],
+            'font': {
+                'color': colors['text']
+            }
+        }
+    }
+
+
+def get_graph(df, forecasted, *args, **kwargs):
+    return dcc.Graph(id='plot', figure=get_figure_to_future(df, forecasted))
+    # return dcc.Graph(id='plot', figure=get_figure_train_test(df, train_size=0.8))
+
+
+def get_metrics(df, n_harm=10000, train_size=0.8):
+    metrics = forecast_train_test_df(df, n_harm=n_harm, train_size=train_size)
+    return html.Div([
+        html.P(f"MAPE: {metrics['MAPE']}", className='text'),
+        html.P(f"MAE: {metrics['MAE']}", className='text'),
+        html.P(f"MSE: {metrics['MSE']}", className='text')]
+    )
 
 
 # TODO: Remove global variables
 n_pred = 300
-harm = 10000
-df = load_data()
+harm = 100000
+train_size = 0.99
+df = load_data(start_from=80000)
 app = dash.Dash(__name__)
 forecasted = forecast(n_pred=n_pred, harm=harm)
 
 app.layout = html.Div(children=[
     html.H1('BTC Price Analysis', className='text'),
     get_input_block(),
-    get_graph(df, forecasted)], id='main-container')
+    get_graph(df, forecasted),
+    get_metrics(df, n_harm=harm, train_size=train_size)
+], id='main-container')
 
 
 @app.callback(
@@ -83,7 +116,9 @@ def update_harm_number(new_harm=None, new_n_pred=None):
         new_n_pred = int(new_n_pred)
         n_pred = new_n_pred
     forecasted = forecast(n_pred=harm, harm=n_pred)
-    return get_figure(df, forecasted)
+    return get_figure_to_future(df, forecasted)
+    # train-test graph
+    # return get_figure_train_test(df, train_size=train_size)
 
 
 if __name__ == '__main__':
