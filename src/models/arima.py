d199b4f19e6308c2b1bc6a564da4f3450c63abc9
Svyatoslav <kovalev.svyatoslav42@gmail.com>
2020-11-19 20:11:27 +0300

    add arima to models

--- src/models/arima.py
+++ src/models/arima.py
@@ -1,43 +1,59 @@
 import pmdarima as pmd
 import pandas as pd
+
+from src.models.model import Model
 from src.utils import transform_date_start
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
-def fit_model(pivoted_df, n):
+class ARIMA(Model):
+
+    def __init__(self, df, n=14, column_name='price', verbose=False, **kwargs):
+        super(ARIMA, self).__init__(n=n, verbose=verbose)
+        self.column_name = column_name
+        self.model = fit_model(df, self.n, self.column_name)
+
+    def predict(self, df):
+        return predict_model(self.model, df, self.n, self.column_name)
+
+    def predict_for_report(self, X, date_start, date_end):
+        return arima_predict_for_report(X, date_start, date_end, self.n, self.column_name)
+
+
+def fit_model(pivoted_df, n, column_name):
     train = pivoted_df.iloc[:-n]
-    train_ex = train.drop(['price'], axis=1)
-    model = pmd.auto_arima(train['price'], train_ex)
+    train_ex = train.drop([column_name], axis=1)
+    model = pmd.auto_arima(train[column_name], train_ex)
     return model
 
 
-def predict_model(model, pivoted_df, n):
+def predict_model(model, pivoted_df, n, column_name):
     train = pivoted_df.iloc[:-n]
-    last_val = train.drop(['price'], axis=1).values[-1].reshape(1, -1).tolist()
+    last_val = train.drop([column_name], axis=1).values[-1].reshape(1, -1).tolist()
     last_ex = last_val * n
     return model.predict(n, last_ex)
 
 
-def update_model(model, pivoted_df, n):
+def update_model(model, pivoted_df, n, column_name):
     train = pivoted_df.iloc[:-n]
-    new_price = train['price'].values[-1]
-    new_val = train.drop(['price'], axis=1).values[-1].reshape(1, -1).tolist()
+    new_price = train[column_name].values[-1]
+    new_val = train.drop([column_name], axis=1).values[-1].reshape(1, -1).tolist()
     model.update(new_price, new_val)
     return model
 
 
-def arima_predict_for_report(df, start_date, end_date, n):
+def arima_predict_for_report(df, start_date, end_date, n, column_name):
     pivoted_df = df[:start_date]
 
-    model = fit_model(pivoted_df, n)
+    model = fit_model(pivoted_df, n, column_name)
     predictions = []
 
     for pivot in pd.date_range(start_date, end_date):
         pivoted_df = df[:pivot]
-        model = update_model(model, pivoted_df, n)
-        prediction = predict_model(model, pivoted_df, n)
+        model = update_model(model, pivoted_df, n, column_name)
+        prediction = predict_model(model, pivoted_df, n, column_name)
         predictions.append(prediction)
 
     columns = [f'n{i + 1}' for i in range(n)]
