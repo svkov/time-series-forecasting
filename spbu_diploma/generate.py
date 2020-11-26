d6fa10b9568db920f4ce10504083f21371957c59
Svyatoslav <kovalev.svyatoslav42@gmail.com>
2020-11-26 13:17:06 +0300

    add text to tex

--- spbu_diploma/generate.py
+++ spbu_diploma/generate.py
@@ -2,7 +2,11 @@ from pdflatex import PDFLaTeX
 import os
 
 
-if __name__ == '__main__':
+def generate():
     os.chdir('spbu_diploma/')
     pdfl = PDFLaTeX.from_texfile('main_example.tex')
     pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=True)
+
+
+if __name__ == '__main__':
+    generate()
