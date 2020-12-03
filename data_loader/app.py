23bc0c8a85a489e56db8ce193e050466d35828ca
Svyatoslav <kovalev.svyatoslav42@gmail.com>
2020-12-03 18:42:19 +0300

    tablename

--- data_loader/app.py
+++ data_loader/app.py
@@ -17,6 +17,7 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=False, nullable=False)
     email = db.Column(db.String, unique=False, nullable=False)
