5b98bc9748d03c3da80fea06c40cc9adf6a3251f
Svyatoslav <kovalev.svyatoslav42@gmail.com>
2020-12-03 11:51:53 +0300

    download data

--- data_loader/app.py
+++ data_loader/app.py
@@ -1,9 +1,12 @@
 import time
 
 from flask import Flask
+from flask_restful import Resource, Api
+import yfinance as yf
 import redis
 
 app = Flask(__name__)
+api = Api(app)
 cache = redis.Redis(host='redis', port=6379)
 
 
@@ -19,11 +22,23 @@ def get_hit_count():
             time.sleep(0.5)
 
 
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+def download_ticker(ticker, period='3y', interval='1d'):
+    return yf.Ticker(ticker).history(period=period, interval=interval)
 
+
+class DataDownload(Resource):
+    def get(self):
+        df = download_ticker('BTC-USD', '1y')
+        return {'data': df.to_json()}
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hit_count': get_hit_count()}
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(DataDownload, '/data/')
 
 if __name__ == '__main__':
     app.run()
