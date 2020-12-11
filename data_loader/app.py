3bd4e87a585e0c17d09a5c5df60db48e06c30b0e
Svyatoslav <kovalev.svyatoslav42@gmail.com>
2020-12-11 15:27:17 +0300

    add cors

--- data_loader/app.py
+++ data_loader/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
 from db_config import DATABASE_CONNECTION_URI
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
+CORS(app)
 
 db = SQLAlchemy(app)
 from routes import route
